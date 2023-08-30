c07f473e8342e1d73e148e514813a2a5f44331fe
avreliakotta <111601503+avreliakotta@users.noreply.github.com>
2023-08-30 21:45:29 +0300

    Deleted jsconfig.json

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import { Profile } from 'components/Profile/Profile';
-import { Statistics } from 'components/Statistics/Statistics';
-import { FriendList } from 'components/FriendList/FriendList';
-import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
+import { Profile } from './components/Profile/Profile';
+import { Statistics } from './components/Statistics/Statistics';
+import { FriendList } from './components/FriendList/FriendList';
+import { TransactionHistory } from './components/TransactionHistory/TransactionHistory';
 import user from './data/user.json';
 import data from './data/data.json';
 import friends from './data/friends.json';
